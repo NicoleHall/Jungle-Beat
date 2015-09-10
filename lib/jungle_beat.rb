590415f2338c53e3f02b2d25f9cf544ae78ffb4a
Nicole Hall <nicolealexandrahall@gmail.com>
2015-09-10 15:50:04 -0600

    in progress

---
+++ lib/jungle_beat.rb
@@ -0,0 +1,117 @@
+class JungleBeat
+  attr_accessor :head, :tail
+
+  def initialize(beats)
+    @head = Node.new(beats)
+  end
+
+  def append(beats)
+    beats = beats.split
+    beats.each do |beat|
+      tail.next_node = Node.new(beat)
+    end
+    beats.count
+  end
+
+  def prepend(beats)
+    beats = beats.split.reverse
+    beats = beats.map do |beat|
+      previous_head = @head
+      @head = Node.new(beat)
+      @head.next_node = previous_head
+    end
+    beats.count + 1
+  end
+
+  def tail
+    current_node = @head
+    while current_node.next_node #until current_node.next_node.nil?
+      current_node = current_node.next_node
+    end
+    current_node
+  end
+
+  def include?(beat)
+    current_node = @head
+    while current_node
+      if current_node.beat == beat
+        return true
+      end
+      current_node = current_node.next_node
+    end
+  end
+
+  def all
+    list_of_beats = []
+    current_node = @head
+    while current_node
+      list_of_beats << current_node.beat
+      current_node = current_node.next_node
+    end
+    list_of_beats.join(" ")
+  end
+
+  def count
+    list_of_beats = []
+    current_node = @head
+    while current_node
+      list_of_beats << current_node.beat
+      current_node = current_node.next_node
+    end
+    list_of_beats.count
+  end
+
+  def remove_last_beat
+    current_node = @head
+    while current_node.next_node.next_node #until this condition is false, do the loop & the condition is looking two ahead
+      current_node = current_node.next_node
+    end
+    removed_node = current_node.next_node
+    current_node.next_node = nil
+    removed_node.beat
+  end
+
+  def pop(num)
+    result = []
+    num.times do
+      result << remove_last_beat
+    end
+    result.reverse.join(" ")
+  end
+
+  def find(position, num_of_beats_to_find)
+    found = []
+    all.split.each_with_index do |value, index|
+      if (index >= position && index < position + num_of_beats_to_find)
+        found << value
+      end
+    end
+    found.join
+  end
+
+  # def insert(position, beat)
+  #   current_node = @head
+  #   counter = 0
+  #   while counter != position
+  #     current_node = current_node.next_node
+  #     counter += 1
+  #     if counter == position
+  #       current_node.next_node = Node.new(beat)
+  #     end
+  #   end
+  # end
+
+end
+
+class Node
+  attr_accessor :beat, :next_node
+
+  def initialize(beat)
+    @beat = beat
+    @next_node = nil
+    #a brand new node won't have a next_node
+  end
+end
+
+
+#input_filename  = ARGV[0]
---
+++ test/jungle_beat_test.rb
@@ -0,0 +1,121 @@
+require_relative '../lib/jungle_beat'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class JungleBeatTest < Minitest::Test
+
+  def test_there_is_a_Jungle_Beat_class
+    assert JungleBeat
+  end
+
+  def test_a_new_Jungle_Beat_is_initialized_with_a_head
+    jb = JungleBeat.new("Beep")
+    assert_equal "Beep", jb.head.beat
+  end
+
+  def test_it_can_append_a_new_node
+    jb = JungleBeat.new("Beep")
+    assert_equal jb.append("Bop"), 1
+    assert_equal "Bop", jb.tail.beat
+  end
+
+  def test_it_can_append_two_new_nodes
+    jb = JungleBeat.new("Beep")
+    assert_equal jb.append("Bop Dee"), 2
+    assert_equal "Dee", jb.tail.beat
+  end
+
+  def test_the_head_is_the_tail_on_a_Jungle_Beat_with_1_item
+    jb = JungleBeat.new("Beep")
+    assert_equal "Beep", jb.tail.beat
+  end
+
+  def test_it_can_prepend_a_new_node
+    jb = JungleBeat.new("Beep")
+    assert_equal 2, jb.prepend("Dee")
+  end
+
+  def test_it_can_prepend_a_two_nodes
+    jb = JungleBeat.new("Beep")
+    assert_equal 3, jb.prepend("Dee Da")
+    assert_equal "Beep", jb.tail.beat
+    assert_equal "Dee", jb.head.beat
+  end
+
+  def test_include_evaluates_to_true_when_beat_is_present_and_false_when_it_is_not_presetn
+    jb = JungleBeat.new("Beep")
+    assert jb.include?("Beep")
+    refute jb.include?("Da")
+  end
+
+  def test_include_works_with_prepend_and_append
+    jb = JungleBeat.new("Beep")
+    jb.append("Um Dee Zee")
+    assert jb.include?("Beep")
+    assert jb.include?("Zee")
+    refute jb.include?("Da")
+  end
+
+  def test_all_returns_all_beats_in_order
+    jb = JungleBeat.new("Beep")
+    jb.append("Um Dee Zee")
+    jb.prepend("Deep")
+    assert_equal "Deep Beep Um Dee Zee", jb.all
+  end
+
+  def test_count_adds_up_all_of_the_beats
+    jb = JungleBeat.new("Beep")
+    jb.append("Um Dee Zee")
+    jb.prepend("Deep")
+    jb.prepend("Deep Deep")
+    assert_equal 7, jb.count
+  end
+
+  def test_remove_last_beat_removes_one_item
+    jb = JungleBeat.new("Beep")
+    jb.append("La")
+    jb.append("Zee")
+    assert_equal "Zee", jb.remove_last_beat
+  end
+
+  def test_two_items_can_be_popped_from_the_list
+    jb = JungleBeat.new("Beep")
+    jb.append("Zee")
+    jb.append("Da")
+    assert_equal "Zee Da", jb.pop(2)
+  end
+
+  def test_it_can_find_one_thing_that_is_the_first_thing_in_the_list
+    jb = JungleBeat.new("Beep")
+    jb.append("Zee")
+    jb.append("Da")
+    jb.append("Da")
+    assert_equal "Beep", jb.find(0, 1)
+  end
+
+  # def test_it_can_insert_one_node_at_position_2
+  #   jb = JungleBeat.new("Beep")
+  #   jb.append("Zee")
+  #   jb.append("Da")
+  #   jb.append("Blah")
+  #   assert_equal "Beep Zee Sing Da Blah", (jb.insert(2, "Sing"))
+  # end
+
+  def test_it_can_play
+    jb = JungleBeat.new("Beep")
+    jb.append("ding, dah, oom, oom, ding, oom, oom, oom, ding, dah, oom, oom, ding, dah, oom, oom, ding, dah, oom, oom ")
+    assert_equal 21, jb.count
+    jb.play
+    "Played #{jb.count} sounds from `beats.txt`""
+  end
+
+
+end
+
+class NodeTest < Minitest::Test
+
+  def test_there_is_a_Node_class
+    assert Node
+  end
+
+end
